f9e294d88df0339fb9e385f1e290d49c22c4fcb8
Isaiah Clement <bed-com-25-18@cc.ac.mw>
2020-10-19 14:54:26 +0200

    added updated codes

--- Student.java
+++ Student.java
@@ -1,11 +1,11 @@
 public class Student {
 
-    private String name;
-    private static int numOfStudents;
+    private String name ;
+    private static int numOfStudents ;
     private double [] grades;
-    private double averageGrades;
+    // private double averageGrades;
   
-// constructor
+    // constructor
     public Student (){
         numOfStudents ++;
     }
@@ -14,38 +14,49 @@ public class Student {
     public void setName (String name) {
         this.name = name;
     }
+
     //method for getting name
     public String getName() {
         return name;
     }
+
     //method for setting grades
     public void setGrades (double [] grades){
 
-        for (int i = 0; i < grades.length; i++) {
-            grades[i] = grades [i];
-        }
+            this.grades = grades; 
     }
+
     //method for accessing grde
     public double [] getGrades () {
         return grades;
     }
+
     //method for getting number of students
     public int getNumberOfStudents () {
         return numOfStudents;
     }
+
     //method for calculating area
-    public double average (double [] grades) {
+    public double getAverageGrades () {
+        
         double sum = 0;
         for (int i = 0; i < grades.length; i ++) {
             sum += grades [i];
         }
-
-       averageGrades = sum/grades.length; 
+        //calculating averrage grades
+       double averageGrades = (int)((sum/grades.length) * 100) /100.0; 
         return averageGrades;
     }
+
     //method for printing
     public void printStudentDetails (){
-      
-        System.out.println ( " Student name is "+ name + " average grades is "+ averageGrades + " and the current number of students is "+ numOfStudents);
+
+        System.out.println();
+        System.out.println ( " Student name is "+ getName() + " average grades is "+ getAverageGrades()+" and the current number of students is "+ numOfStudents);
+        System.out.println();
+        System.out.println();
+
+    
     }
+
 }
--- StudentImpliment.java
+++ StudentImpliment.java
@@ -3,53 +3,53 @@ import java.util.Scanner;
 public class StudentImpliment {
     public static void main (String [] args) {
 
-        // creating a scanner object
-        Scanner don = new Scanner(System.in);
+             // creating a scanner object
+             Scanner input = new Scanner(System.in);
 
-        //prompting to enter name
-        System.out.print (" Enter Your name : ");
-        String name = don.nextLine ();
+            System.out.println();
+            System.out.print (" How many students are you going to enter? : ");
+            int k = input.nextInt();
+          
+        for (int i = 1; i <= k; i++) {
 
-        // creating a grades array for holding three student's grades
-        double [] grades = new double [3];
-        System.out.print (" Enter Your 3 grades : ");
-       for (int i = 0; i < grades.length; i ++) {
-           grades [i] = don.nextDouble();
-       }
-        // creatin object for first student
-       Student student1 = new Student ();
-      
-      //Sending the data to Student Class
-      student1.setName(name);
-      student1.setGrades(grades);
-      student1.average(grades);
+             // creatin object for student
+             Student student = new Student ();
+             mainput(student, i);
 
-        //printing the results
-      student1.printStudentDetails();
-
-
-        // pronting user to enter name of the second student
-        System.out.print (" Enter Your name : ");
-         name = don.nextLine ();
-        
-        //prompting to enter grades
-          System.out.print (" Enter Your 3 grades : ");
-       for (int i = 0; i < grades.length; i ++) {
-           grades [i] = don.nextDouble();
-       }
-
-        // creating a new object for the second student
-        Student student2 = new Student ();
-
-        // Sending second students data to Student class
-      student2.setName(name);
-      student2.setGrades(grades);
-      student2.average(grades);
-
-        //printing second students details
-      student2.printStudentDetails();
-      
-
-
+        }
+         
     }
+
+
+        public static void mainput (Student c, int k) {
+
+            // creating a scanner object
+             Scanner don = new Scanner(System.in);
+
+
+             String name;
+
+             //prompting to enter name
+             System.out.print (" Enter name of the student"+ k +" : ");
+             name = don.next();
+
+            // creating a grades array for holding three student's grades
+             double [] grades = new double [3];
+             System.out.print (" Enter all 3 student"+ k +"'s grades : ");
+             for (int i = 0; i < grades.length; i ++) {
+            grades [i] = don.nextDouble();
+       }
+
+            //Sending the data to Student Class
+             c.setName(name);
+             c.setGrades(grades);
+          
+            //printing the results
+             c.printStudentDetails();
+
+        }
+           
+           
+
+
 }
